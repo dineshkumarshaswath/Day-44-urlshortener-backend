25609ac601e690c2a31f411ad025d28702b4ee1f
dineshkumarshaswath <dineshshaswath@gmail.com>
2023-08-05 15:26:59 +0530

    authdelete

--- routers/urlrouter.js
+++ routers/urlrouter.js
@@ -7,7 +7,7 @@ const { postUrl, getUrl, clickUrl, deleteUrl, myUrl } = require("../controllers/
 const router=express.Router()
 
 router.route("/post/url").post(isAuthenticated,postUrl)
-router.route("/redirect/:shortId").get(isAuthenticated,getUrl)
+router.route("/redirect/:shortId").get(getUrl)
 router.route("/total/:shortId").get(isAuthenticated,clickUrl)
 router.route("/delete/:id").delete(isAuthenticated,deleteUrl)
 router.route("/myurl").get(isAuthenticated,myUrl)
